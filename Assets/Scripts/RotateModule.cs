a3b91195c4e7cbd032d8722af23f1d2128d50e77
harry1878 <60026408+harry1878@users.noreply.github.com>
2020-01-31 17:11:54 +0900

    homework

--- Assets/Scripts/RotateModule.cs
+++ Assets/Scripts/RotateModule.cs
@@ -10,10 +10,11 @@ class RotateModule : MonoBehaviour
     //1++. 행성계 곡선으로 구현하기
     //2. isRotate 구현하기
 
-    public float speed = 10;
+    public float speed = 1;
     [SerializeField] private bool isRotate = false;
 
 
+    
     private void Update()
     {
         transform.RotateAround(Vector3.zero, new Vector3(0 ,0 ,1), speed);
